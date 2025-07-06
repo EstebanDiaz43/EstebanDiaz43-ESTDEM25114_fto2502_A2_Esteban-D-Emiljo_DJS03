2814a689d5e0fa80f8d948631352ad3ff9740a4d
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-07-06 16:08:23 +0200

    Added salution files

--- ESTDEM25114_fto2502_A2_Esteban-D-Emiljo_DJS03/src/main.jsx
+++ ESTDEM25114_fto2502_A2_Esteban-D-Emiljo_DJS03/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("app-root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DataFetcher />
+  </StrictMode>
+);
