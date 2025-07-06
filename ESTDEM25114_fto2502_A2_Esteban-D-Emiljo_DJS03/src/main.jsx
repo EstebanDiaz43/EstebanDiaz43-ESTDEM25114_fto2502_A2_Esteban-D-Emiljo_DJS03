98c7931cdba8184298da0da8425b5bc97502ff49
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-07-06 16:09:49 +0200

    Added datafetcher function

--- ESTDEM25114_fto2502_A2_Esteban-D-Emiljo_DJS03/src/main.jsx
+++ ESTDEM25114_fto2502_A2_Esteban-D-Emiljo_DJS03/src/main.jsx
@@ -2,6 +2,20 @@ import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+const DataFetcher = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://podcast-api.netlify.app/")
+      .then((res) => res.json())
+      .then((apiData) => {
+        setData(apiData); // <-- Use the response directly
+        setLoading(false);
+      });
+  }, []);
+};
+
 createRoot(document.getElementById("app-root")).render(
   <StrictMode>
     <DataFetcher />
