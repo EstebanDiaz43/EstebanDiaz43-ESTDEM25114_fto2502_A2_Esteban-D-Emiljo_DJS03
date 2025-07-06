ce740d75aed64764ffa54c3671f005ca5d59acd9
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-07-06 17:26:23 +0200

    Added js function to turn the updated data into a readable format

--- ESTDEM25114_fto2502_A2_Esteban-D-Emiljo_DJS03/src/main.jsx
+++ ESTDEM25114_fto2502_A2_Esteban-D-Emiljo_DJS03/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { DateUtils } from "../utils/DateUtils";
 import "./styles.css";
 
 const DataFetcher = () => {
@@ -18,16 +19,15 @@ const DataFetcher = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div className="container">
+    <div className="grid" id="podcastGrid">
       {data.length === 0 && <div>No data found.</div>}
       {data.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} className="card">
           <img src={item.image} alt={item.title} />
-          <h2>{item.title}</h2>
-          <p>{item.description}</p>
+          <h3>{item.title}</h3>
           <p>{item.seasons}</p>
           <p>{item.genres}</p>
-          <p>{item.updated}</p>
+          <p>{item.updated ? DateUtils.format(item.updated) : ""}</p>
         </div>
       ))}
     </div>
